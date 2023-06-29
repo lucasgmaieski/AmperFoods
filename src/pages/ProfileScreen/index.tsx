54d14dfa3ee38e739f55da19b20109754cc0e298
Administrador <lucasgrigol@gmail.com>
2023-06-29 18:41:48 -0300

    create user information form

--- src/pages/ProfileScreen/index.tsx
+++ src/pages/ProfileScreen/index.tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../redux/hooks/useAppSelector';
 import * as C from './styled';
-import { setName } from '../../redux/reducers/UserReducer';
+import { setInfo, setName } from '../../redux/reducers/UserReducer';
 import { Header } from '../../components/Header';
 
 
@@ -11,19 +11,79 @@ export const ProfileScreen = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [headerSearch, setHeaderSerach] = useState('');
+    const [nameInput, setNameInput] = useState('');
+    const [emailInput, setEmailInput] = useState('');
+    const [addressInput, setAddressInput] = useState('');
+    const [phoneInput, setPhoneInput] = useState('');
+    const [saveSuccess, setSaveSuccess] = useState(false);
 
-    const name = useAppSelector(state => state.persistedReducer.user.name);
+    const userInfos = useAppSelector(state => state.persistedReducer.user);
 
-    const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
-        dispatch( setName({name: e.target.value}));
-        console.log(name);
+    useEffect(()=>{
+        setUserInfos();
+    }, [])
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.target.id == 'name') {
+            setNameInput(e.target.value);
+        }
+        if(e.target.id == 'phone') {
+            setPhoneInput(e.target.value);
+        }
+        if(e.target.id == 'email') {
+            setEmailInput(e.target.value);
+        }
+        if(e.target.id == 'address') {
+            setAddressInput(e.target.value);
+        }
+    }
+
+    const handleSaveButton = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch( setInfo({
+            name: nameInput,
+            email: emailInput,
+            phone: phoneInput,
+            address: addressInput
+        }));
+        setUserInfos();
+        setSaveSuccess(true);
+    }
+
+    const setUserInfos = () => {
+        setNameInput(userInfos.name);
+        setAddressInput(userInfos.address);
+        setEmailInput(userInfos.email);
+        setPhoneInput(userInfos.phone);
     }
 
     return (
         <C.Container>
             <Header search={headerSearch} onSearch={setHeaderSerach}/>
 
-            <C.Titulo>Profile</C.Titulo>
+            <C.Titulo>Perfil do usuário</C.Titulo>
+            <C.FormArea onSubmit={handleSaveButton}>
+                <C.Label>
+                    Nome:
+                    <C.Input type="text" id="name" value={nameInput} onChange={handleInputChange}/>
+                </C.Label>
+                <C.Label>
+                    Email:
+                    <C.Input type="email" id="email" value={emailInput} onChange={handleInputChange}/>
+                </C.Label>
+                <C.Label>
+                    Telefone:
+                    <C.Input type="tel" id="phone" value={phoneInput} onChange={handleInputChange}/>
+                </C.Label>
+                <C.Label>
+                    Endereço:
+                    <C.Input type="text" id="address" value={addressInput} onChange={handleInputChange}/>
+                </C.Label>
+                <C.ButtonSave type="submit" value="salvar"/>
+                {saveSuccess && 
+                    <h4>Salvo com sucesso.</h4>
+                }
+            </C.FormArea>
 
         </C.Container>
     );
--- src/pages/ProfileScreen/styled.ts
+++ src/pages/ProfileScreen/styled.ts
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100%;
+    margin: 15px;
+`;
 
-export const Titulo = styled.h1``;
+export const Titulo = styled.div`
+    font-size: 20px;
+    color: #fff;
+    margin: 20px;
+`;
+export const FormArea = styled.form`
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 20px;
+    max-width: 400px;
+`;
+export const Label = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin-bottom: 25px;
+    color: #FA8229;
+`;
+export const Input = styled.input`
+    padding: 10px 10px;
+    border: none;
+    outline: none;
+    background-color: #f9f9f9;
+    border-bottom: 1px solid #FA8229;
+`;
+export const ButtonSave = styled.input`
+    width: 100%;
+    margin: auto;
+    display: block;
+    background-color: #FA8229;
+    border: none;
+    padding: 10px;
+    color: #fff;
+    border-radius:10px;
+    cursor: pointer;
+`;
